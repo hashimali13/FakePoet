1342558d6b566420dfac889ebee092f3c33f8ad7
yeezysenpai <hashimwars@gmail.com>
2020-05-02 18:35:59 +0100

    Initial commit

---
+++ src/sample/Word.java
@@ -0,0 +1,19 @@
+package sample;
+
+import java.util.ArrayList;
+
+public class Word {
+    String word;
+    ArrayList<String> next;
+    public Word(){
+        word="test";
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public ArrayList<String> getNext() {
+        return next;
+    }
+}
