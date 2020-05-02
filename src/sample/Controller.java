5ca68118015f84f34654914506107c37c6b180bf
yeezysenpai <hashimwars@gmail.com>
2020-05-02 18:45:42 +0100

    add button to gui

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
 public class Controller {
+    public Label helloWorld;
+    Word word;
+
+    public Controller(){
+        word = new Word();
+    }
+
+    public void sayHelloWorld(ActionEvent actionEvent) {
+        System.out.println("aaaaa");
+        helloWorld.setText(word.getWord());
+
+    }
 }
