43ce1f2220c62efe20a077305ec0b3a0c1bc283a
yeezysenpai <hashimwars@gmail.com>
2020-05-02 19:46:28 +0100

    process tolkein.txt

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.*;
+
+import java.util.ArrayList;
+
 public class Main extends Application {
 
     @Override
@@ -17,9 +21,29 @@ public class Main extends Application {
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
         Word word = new Word();
         System.out.println(word.getWord());
+        ArrayList<String> ch = main.getWords();
         launch(args);
+    }
+
+    public ArrayList<String> getWords() throws IOException {
+       ArrayList<String> words = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(
+                new FileReader("C:\\Users\\hashi\\Documents\\work\\cc\\FakePoet\\res\\tolkein.txt"));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] wordSplit = line.split(" ");
+            for(String string: wordSplit){
+                if(string.length()>0){
+                    words.add(string);
+                }
+            }
+        }
+        System.out.println(words.toString());
+       return words;
+
     }
 }
